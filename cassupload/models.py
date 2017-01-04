a87cbb42bb09d9dff86fd5a71fb2b2366e134793
Joshua Prince <jtprince@ucdavis.edu>
2017-01-03 23:25:09 -0800

    [cassupload] Fix Model serialization

--- cassupload/models.py
+++ cassupload/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from .validators import validate_extension, validate_size
 
 
+def upl_to(instance, fn):
+    return os.path.join('sound', instance.name + '.mp3')
+
+
 class Guild(models.Model):
     snowflake = models.CharField(max_length=64)
 
@@ -14,7 +18,7 @@ class Guild(models.Model):
 class Sound(models.Model):
     name = models.CharField(max_length=32, default='', unique=True)
     file = models.FileField(
-        upload_to=lambda instance, fn: os.path.join('sound', instance.name + '.mp3'),
+        upload_to=upl_to,
         validators=[validate_extension, validate_size]
     )
     play_count = models.IntegerField(default=0)  # TODO this doesn't do anything yet
