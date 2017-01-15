0cde540ef7aa488eec675cf11cecb780de17800c
Joshua Prince <jtprince@ucdavis.edu>
2017-01-15 03:03:47 -0800

    [casslist] Show total play count

--- casslist/views.py
+++ casslist/views.py
@@ -1,4 +1,5 @@
 from django.views import generic
+from django.db.models import Sum
 
 from cassupload import models
 
@@ -6,6 +7,8 @@ from cassupload import models
 class CassListView(generic.ListView):
     template_name = 'casslist/index.html'
     context_object_name = 'cass_sound_list'
+
+    total_plays = models.Sound.objects.all().aggregate(Sum('play_count'))['play_count__sum']
 
     def get_queryset(self):
         return models.Sound.objects.order_by('-id')
