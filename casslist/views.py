fd0dad58403f34338b85edd83641e65a68779705
Joshua Prince <jtprince@ucdavis.edu>
2017-07-24 00:57:25 -0700

    [casslist] Fix error when making first migrations in a new project

--- casslist/views.py
+++ casslist/views.py
@@ -1,3 +1,4 @@
+from django.db import OperationalError
 from django.views import generic
 from django.db.models import Sum
 
@@ -8,7 +9,10 @@ class CassListView(generic.ListView):
     template_name = 'casslist/index.html'
     context_object_name = 'cass_sound_list'
 
-    total_plays = models.Sound.objects.all().aggregate(Sum('play_count'))['play_count__sum']
+    try:
+        total_plays = models.Sound.objects.all().aggregate(Sum('play_count'))['play_count__sum']
+    except OperationalError:  # The database is empty.
+        total_plays = 0
 
     def get_queryset(self):
         return models.Sound.objects.order_by('-id')
